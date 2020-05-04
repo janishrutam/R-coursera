6ee72e704c07c1af937fcee6c817ebddda6649a0
janishrutam <janishrutam@gmail.com>
2020-05-04 12:42:02 +0530

    new

--- testing.R
+++ testing.R
@@ -2,3 +2,7 @@ print("this file was created with Rstudio")
 print("Add now it lives on github")
 print("owner is shrutam jani")
 print("shrutam has completed his graduation in mechanical engineering")
+
+install.packages("rmarkdown")
+library(rmarkdown)
+tinytex::install_tinytex()
